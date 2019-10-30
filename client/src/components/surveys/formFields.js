372d126f097605cfe46d956b6f9b6a2f09580033
Chris Bennett <chrisbennett09@gmail.com>
2019-10-30 12:35:27 -0500

    Users can see survey results

---
+++ client/src/components/surveys/formFields.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    label: 'Survey Title',
+    name: 'title',
+    noValueError: 'Title is required'
+  },
+  {
+    label: 'Subject Line',
+    name: 'subject',
+    noValueError: 'Subject is required'
+  },
+  {
+    label: 'Email Body',
+    name: 'body',
+    noValueError: 'Body is required'
+  },
+  {
+    label: 'Recipient List',
+    name: 'recipients',
+    noValueError: 'Email recipient list is requried'
+  }
+];
